f7fe445bb3877753e7228a8376ec4d5d06bb1780
Pedro <Pedro.l2002@hotmail.com>
2019-09-18 22:06:58 -0300

    Notas finalizado

---
+++ app/src/main/java/br/com/etecia/myapplication/Notas_Dois.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Notas_Dois extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas__dois);
+    }
+}
